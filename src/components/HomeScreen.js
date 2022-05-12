6edc795c9107b37bb613e7aa6675deb8dabdc284
LucasLima <lugablima@gmail.com>
2022-05-12 01:25:21 -0300

    Rodapé da tela inicial e atualização no html da quantidade de perguntas respondidas implementadas

--- src/components/HomeScreen.js
+++ src/components/HomeScreen.js
@@ -1,7 +1,13 @@
+import React from "react";
+import Footer from "../shared/Footer";
+
 export default function HomeScreen() {
+    const [qntAnswers, setQntAnswers] = React.useState(0);
+    
     return (
         <div className="home-screen">
-            
+            <Footer qntAnswers={qntAnswers} />
+            <button onClick={() => setQntAnswers(qntAnswers + 1)}>Inc</button>
         </div>
     );
 }
---
+++ src/shared/Footer.js
@@ -0,0 +1,7 @@
+export default function Footer({ qntAnswers }) {
+    return (
+        <div className="footer">
+            <p>{qntAnswers}/4 CONCLUÍDOS</p>
+        </div>
+    );
+}
