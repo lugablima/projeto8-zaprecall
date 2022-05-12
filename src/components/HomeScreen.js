6a0a15da1b543f34ffa6e5cfca834c11b3375294
LucasLima <lugablima@gmail.com>
2022-05-12 01:56:09 -0300

    Header da tela inicial implementado

--- src/components/HomeScreen.js
+++ src/components/HomeScreen.js
@@ -1,11 +1,13 @@
 import React from "react";
 import Footer from "../shared/Footer";
+import Header from "./Header";
 
 export default function HomeScreen() {
     const [qntAnswers, setQntAnswers] = React.useState(0);
     
     return (
         <div className="home-screen">
+            <Header />
             <Footer qntAnswers={qntAnswers} />
             <button onClick={() => setQntAnswers(qntAnswers + 1)}>Inc</button>
         </div>
